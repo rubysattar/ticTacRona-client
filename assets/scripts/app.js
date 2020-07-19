5daaa853838fab87f4089f365dd420942f501f74
rubysattar <rubysattar@gmail.com>
2020-07-18 21:14:45 -0400

    added game event for reset game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,15 +3,18 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/auth_events')
+const gameEvents = require('./game/game_events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-
-//Authentication Listeners
+// Authentication Listeners
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
+
+  // Game listeners
+  $('.endGame').on('click', gameEvents.onReset)
 })
---
+++ assets/scripts/game/game_events.js
@@ -0,0 +1,26 @@
+'use strict'
+const api = require('./game_api')
+const getFormFields = require('../../../get-form-fields')
+const ui = require('./game_ui')
+
+const cells = document.querySelectorAll('.cell')
+
+const onReset = function (event) {
+  event.preventDefault()
+  document.querySelector('.endGame').getElementsByClassName.display = 'none'
+  ui.gameBoard = Array.from(Array(9).keys())
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = ''
+    cells[i].on('click', logClick) 
+  }
+}
+
+const logClick(cell) {
+  // we want this function to pass the id of the cell that was clicked 
+  // to the user's data
+  console.log(cell.target.id)
+}
+
+module.exports = {
+  onReset
+}
