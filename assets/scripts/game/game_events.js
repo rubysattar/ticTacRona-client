a6dee054a76eef8b5dc0c14d875fe01b591fb626
rubysattar <rubysattar@gmail.com>
2020-07-21 10:39:57 -0400

    commented out possible game logic

--- assets/scripts/game/game_events.js
+++ assets/scripts/game/game_events.js
@@ -7,6 +7,8 @@ const gameUi = require('./game_ui')
 const store = require('../store')
 const { createNewGame } = require('./game_api')
 
+let gameBoard
+
 const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,6 +23,7 @@ const winningCombos = [
 const cells = document.querySelectorAll('.cell')
 
 let turn = true
+let wins = 0
 
 const onCreateGame = function () {
   gameApi.createNewGame()
@@ -65,12 +68,51 @@ const onUpdateGameState = function (clickEvent) {
     turn = !turn
   }
   // need to come up with game logic for game over
+
+  // if (checkWin(player === true)) {
+  //   wins++
+  //   $('#endgameMessage').text('Congrats! you won the game!')
+  //   $('.score-for-x').innerText(wins)
+
     return turn
   }
+
+// This is one way to check for winner.. is incomplete
+
+  // function checkWin(gameBoard, player) {
+  //   let plays = gameBoard.reduce((a, e, i) =>
+  //   (e === player)) ? a.concat(i) : a, []);
+  //   let gameWon = null
+  // }
+
+// This is a giant if else statement to check for winner
+
+// const checkWin = function (hasText) {
+//   if ($('.m3').text(hasText) && $('.m1').text(hasText) && $('.m2').text(hasText)) {
+//   return true
+//   } else if ($('.t1').text(hasText) && $('.t2').text(hasText) && $('.t3').text(hasText)) {
+//   return true
+//   } else if ($('.b1').text(hasText) && $('.b2').text(hasText) && $('.b3').text(hasText)) {
+//   return true
+//   } else if ($('.t1').text(hasText) && $('.m1').text(hasText) && $('.b1').text(hasText)) {
+//   return true
+//   } else if ($('.t2').text(hasText) && $('.m2').text(hasText) && $('.b2').text(hasText)) {
+//   return true
+//   } else if ($('.t3').text(hasText) && $('.m3').text(hasText) && $('.b3').text(hasText)) {
+//   return true
+//   } else if ($('.t1').text(hasText) && $('.m2').text(hasText) && $('.b3').text(hasText)) {
+//   return true
+//   } else if ($('.b1').text(hasText) && $('.m2').text(hasText) && $('.t3').text(hasText)) {
+//   return true
+//   } else {
+//   return false
+//   }
+// }
 
 module.exports = {
   onReset,
   onUpdateGameState,
   onCreateGame,
   winningCombos
+  // checkWin
 }
